f1044c22ba852b038d3a91168e61d5234c3292ad
unknown <youremailid@example.com>
2018-09-08 20:44:33 +0800

    注释更改

--- cl/cl/pipelines.py
+++ cl/cl/pipelines.py
@@ -9,23 +9,23 @@ class ClPipeline(object):
         pass
         
     def process_item(self, item, spider):
-        os.chdir('E:\\Temp\\nmsl')
-        if os.path.exists(item['title']):
-            os.chdir('E:\\Temp\\nmsl\\'+str(item['title']))
-            text1=open('C:\\Users\\xjh\\Desktop\\123\\nmsl.txt','w')
-            text1.write('进入成功')
-            text1.close()
+        os.chdir('E:\\Temp\\nmsl')  #返回目标存储根目录
+            if os.path.exists(item['title']): #目标判断文件夹是否存在
+            os.chdir('E:\\Temp\\nmsl\\'+str(item['title'])) #进入
+            #text1=open('C:\\Users\\k\\Desktop\\123\\nmsl.txt','w')#测试是否进入文件夹
+            #text1.write('进入成功')
+            #text1.close()
         else:
-            os.mkdir('E:\\Temp\\nmsl\\'+item['title'])
+            os.mkdir('E:\\Temp\\nmsl\\'+item['title']) #建立文件夹，建立绝对路径，相对路径建立无法进入成功
             os.chdir('E:\\Temp\\nmsl\\'+str(item['title']))
-            text2=open('C:\\Users\\xjh\\Desktop\\123\\nmsl2.txt','w')
-            text2.write('进入成功')
-            text2.close()
+            #text2=open('C:\\Users\\k\\Desktop\\123\\nmsl2.txt','w') #测试是否能进入文件夹
+            #text2.write('进入成功')
+            #text2.close()
         for n in range(len(item['url'])):
-            text3=open('C:\\Users\\xjh\\Desktop\\123\\nmsl3.txt','w')
-            text3.write(str(item['url'][n]))
-            text3.close()
-            r = requests.request('get',item['url'][n])
+            #text3=open('C:\\Users\\k\\Desktop\\123\\nmsl3.txt','w') #测试是否能循环
+            #text3.write(str(item['url'][n]))
+            #text3.close()
+            r = requests.request('get',item['url'][n]) #下载图片
             f = open('%d.jpg'%n, 'ab')
             f.write(r.content)
             f.close()
--- cl/cl/spiders/cl.py
+++ cl/cl/spiders/cl.py
@@ -5,28 +5,28 @@ import re
 
 class clspider(scrapy.spiders.Spider):
     name='cl'
-    start_urls=['https://www.t66y.com/thread0806.php?fid=16&search=&page=1']
+    start_urls=['https://www.t66y.com/thread0806.php?fid=16&search=&page=1'] #初始网址
     def parse(self,response):
-        first=response.xpath('//tr/td/h3/a')
+        first=response.xpath('//tr/td/h3/a') 
 #提取子网站列表
         for url_list in first:
             clurl=ClItem()
             try:
-                clurl['url']='https://www.t66y.com/'+url_list.xpath('./@href').extract()[0]
+                clurl['url']='https://www.t66y.com/'+url_list.xpath('./@href').extract()[0]#子网站改造
             except:
                 continue
-            yield scrapy.Request(clurl['url'],meta={'item': clurl},callback=self.get_picture)
+            yield scrapy.Request(clurl['url'],meta={'item': clurl},callback=self.get_picture) #返回子网站
     def get_picture(self,response):
-        picture_list=response.xpath('//p/input/@data-src').extract()
-        picture_title=response.xpath('//title/text()').extract()
-        cl_son=ClItem()
-        cl_son['url']=picture_list
-        p2 = re.compile(r'[^\u4e00-\u9fa5]')
+        picture_list=response.xpath('//p/input/@data-src').extract()#提取子网站图片url
+        picture_title=response.xpath('//title/text()').extract()  #子网站标题
+        cl_son=ClItem()    
+        cl_son['url']=picture_list #赋值
+        p2 = re.compile(r'[^\u4e00-\u9fa5]')#正则提取子网站标题汉字
         zh="".join(p2.split(picture_title[0])).strip()
-        cl_son['title']=zh[0:10]
-        text3=open('C:\\Users\\xjh\\Desktop\\123\\nmsl.txt','a')
-        text3.write(str(cl_son['url']))
-        text3.close()
+        cl_son['title']=zh[0:10]   #文件夹名取前10个
+        #text3=open('C:\\Users\\k\\Desktop\\123\\nmsl.txt','a') #测试cl.py返回管道信息有没有错误
+        #text3.write(str(cl_son['url']))
+        #text3.close()
         yield cl_son
 
 
